91e65aad0e0ac8e48dbd04f389654f047ca79223
MityaAndreevich <dmitry.logachev@icloud.com>
2021-11-30 17:09:57 +0300

    - dismissed mistakes - pickerView added

---
+++ BiteCoinApp/Controllers/MainViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MainViewController.swift
+//  BiteCoinApp
+//
+//  Created by Dmitry Logachev on 30.11.2021.
+//
+
+import UIKit
+
+class MainViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    let coinManager = CoinManager()
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        coinManager.currencyArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        coinManager.currencyArray[row]
+    }
+    
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
+
+    }
+}
